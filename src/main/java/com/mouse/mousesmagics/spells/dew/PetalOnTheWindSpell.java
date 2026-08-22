db24c44ecc5fa4c18d5380e1be8c2cf6634d949b
MousePriest <alexanderjohnson8933@gmail.com>
2026-08-22 21:43:19 +0100

    UwU

--- src/main/java/com/mouse/mousesmagics/spells/dew/PetalOnTheWindSpell.java
+++ src/main/java/com/mouse/mousesmagics/spells/dew/PetalOnTheWindSpell.java
@@ -11,23 +11,26 @@ import io.redspace.ironsspellbooks.api.spells.CastType;
 import io.redspace.ironsspellbooks.api.spells.SpellRarity;
 import io.redspace.ironsspellbooks.api.util.AnimationHolder;
 import io.redspace.ironsspellbooks.api.util.Utils;
-import io.redspace.ironsspellbooks.registries.MobEffectRegistry;
+import io.redspace.ironsspellbooks.capabilities.magic.MagicManager;
+import io.redspace.ironsspellbooks.capabilities.magic.RecastInstance;
+import io.redspace.ironsspellbooks.particle.TraceParticleOptions;
 import io.redspace.ironsspellbooks.registries.SoundRegistry;
-import io.redspace.ironsspellbooks.spells.ender.TeleportSpell;
-import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
-import net.minecraft.sounds.SoundSource;
-import net.minecraft.world.InteractionHand;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.projectile.Projectile;
+import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.Nullable;
+import org.joml.Vector3f;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
@@ -79,51 +82,47 @@ public class PetalOnTheWindSpell extends AbstractSpell {
     }
 
     @Override
-    public void onClientPreCast(Level level, int spellLevel, LivingEntity entity, InteractionHand hand, @Nullable MagicData playerMagicData) {
-        super.onClientPreCast(level, spellLevel, entity, hand, playerMagicData);
-        Vec3 forward = entity.getForward().normalize();
-        for (int i = 0; i < 35; i++) {
-            Vec3 motion = forward.scale(Utils.random.nextDouble() * 3f);
-            level.addParticle(MMParticleHelper.FLOWER, entity.getRandomX(.4f), entity.getRandomY(), entity.getRandomZ(.4f), motion.x, motion.y, motion.z);
-            level.addParticle(MMParticleHelper.FADING_FLOWER, entity.getRandomX(.4f), entity.getRandomY(), entity.getRandomZ(.4f), motion.x, motion.y, motion.z);
-        }
+    public int getRecastCount(int spellLevel, @Nullable LivingEntity entity) {
+        return 5;
     }
 
-    @Override
     public void onCast(Level level, int spellLevel, LivingEntity entity, CastSource castSource, MagicData playerMagicData) {
-        var teleportData = (TeleportSpell.TeleportData) playerMagicData.getAdditionalCastData();
-
-        Vec3 dest = null;
-        if (teleportData != null) {
-            var potentialTarget = teleportData.getTeleportTargetPosition();
-            dest = potentialTarget;
+        var recasts = playerMagicData.getPlayerRecasts();
+        if (!recasts.hasRecastForSpell(getSpellId())) {
+            recasts.addRecast(new RecastInstance(getSpellId(), spellLevel, getRecastCount(spellLevel, entity), 120, castSource, null), playerMagicData);
         }
 
-        if (dest == null) {
-            dest = findTeleportLocation(spellLevel, level, entity);
+        float distance = 15f;
+        Vec3 forward = entity.getForward();
+        Vec3 end = Utils.raycastForBlock(level, entity.getEyePosition(), entity.getEyePosition().add(forward.scale(distance)), ClipContext.Fluid.NONE).getLocation();
+        AABB hitbox = entity.getHitbox().expandTowards(end.subtract(entity.getEyePosition())).inflate(2);
+        var targetableEntities = level.getEntities(entity, hitbox, e ->
+                !e.isSpectator() &&
+                        (e instanceof LivingEntity || e instanceof Projectile) &&
+                        e.getBoundingBox().getCenter().subtract(entity.getBoundingBox().getCenter()).normalize().dot(entity.getForward()) >= .85);
+        targetableEntities.sort(Comparator.comparingDouble(e -> e.distanceToSqr(entity)));
+        if (!targetableEntities.isEmpty() && targetableEntities.get(0).distanceToSqr(entity) < distance * distance) {
+            var closestEntity = targetableEntities.get(0);
         }
+        Vec3 rayVector = end.subtract(entity.getEyePosition());
+        Vec3 impulse = rayVector.scale(1 / 6f).add(0, 0.1, 0);
+        entity.setDeltaMovement(entity.getDeltaMovement().scale(0.2).add(impulse));
+        entity.hurtMarked = true;
+        entity.addEffect(new MobEffectInstance(MobEffects.LEVITATION, 30, 0, false, false, true));
 
-        if (entity.isPassenger()) {
-            entity.stopRiding();
+        int trailParticles = 15;
+        double speed = rayVector.length() / 12.0 * .75;
+        for (int i = 0; i < trailParticles; i++) {
+            Vec3 particleStart = entity.getBoundingBox().getCenter().add(Utils.getRandomVec3(1 + entity.getBbWidth()));
+            Vec3 particleDew = particleStart.add(rayVector);
+            MagicManager.spawnParticles(level, new TraceParticleOptions(Utils.v3f(particleDew), new Vector3f(1f, .333f, 1f)), particleStart.x, particleStart.y, particleStart.z, 1, 0, 0, 0, speed, false);
         }
-        Utils.handleSpellTeleport(this, entity, dest);
-        entity.resetFallDistance();
-        level.playSound(null, dest.x, dest.y, dest.z, getCastFinishSound().get(), SoundSource.NEUTRAL, 1f, 1f);
-
-        entity.addEffect(new MobEffectInstance(MobEffects.SLOW_FALLING, 40, 2, false, false, true));
-        entity.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 60, 2, false, false, true));
-
-        playerMagicData.resetAdditionalCastData();
 
         super.onCast(level, spellLevel, entity, castSource, playerMagicData);
     }
 
-    private Vec3 findTeleportLocation(int spellLevel, Level level, LivingEntity entity) {
-        return TeleportSpell.findTeleportLocation(level, entity, getDistance(spellLevel, entity));
-    }
-
-    private float getDistance(int spellLevel, LivingEntity sourceEntity) {
-        return (float) (Utils.softCapFormula(getEntityPowerMultiplier(sourceEntity)) * getSpellPower(spellLevel, null));
+   public float getDistance(int spellLevel, LivingEntity entity) {
+        return 15f;
     }
 
     @Override
